1c398aae9458901395e46c152f492c79526a50d4
lisantov <71330868+lisantov@users.noreply.github.com>
2024-10-24 12:59:48 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,15 +10,19 @@ LoadGallery();
 function LoadGallery(){
     let i = 0;
     try {
+        console.log("Trying local directory");
         document.querySelectorAll(".grid-block").forEach(function(block) {
             block.style.backgroundImage = "url(" + galleryArray[i] + ")";
             i++;
         });
     } catch(err){
+        console.log("Failed. Trying Github directory");
         i = 0;
         document.querySelectorAll(".grid-block").forEach(function(block) {
-            block.style.backgroundImage = "url(lisantov.github.io/Origami/" + galleryArray[i].substring(3);
+            block.style.backgroundImage = "url(lisantov.github.io/Origami/" + galleryArray[i].substring(3) + ")";
             i++;
         });
     }
+    console.log("Success.");
 }
+
