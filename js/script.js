601a729ede31d43685bb3d9c6d737a0ea02d7b1f
lisantov <71330868+lisantov@users.noreply.github.com>
2024-10-24 12:55:26 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,8 +9,16 @@ LoadGallery();
 
 function LoadGallery(){
     let i = 0;
-    document.querySelectorAll(".grid-block").forEach(function(block) {
-        block.style.backgroundImage = "url("+galleryArray[i]+")";
-        i++;
-    });
+    try {
+        document.querySelectorAll(".grid-block").forEach(function(block) {
+            block.style.backgroundImage = "url(" + galleryArray[i] + ")";
+            i++;
+        });
+    } catch(err){
+        i = 0;
+        document.querySelectorAll(".grid-block").forEach(function(block) {
+            block.style.backgroundImage = "url(lisantov.github.io/Origami/" + galleryArray[i].substring(3);
+            i++;
+        });
+    }
 }
