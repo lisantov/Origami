f15cdbbdf4d9b2d2c157593a0c7925141cd2f91e
lisantov <71330868+lisantov@users.noreply.github.com>
2024-10-26 00:38:35 +0700

    Add files via upload

---
+++ js/goTop.js
@@ -0,0 +1,18 @@
+let button = document.querySelector('.to-top'); 
+let isShowing = false;
+
+function CheckScroll(ev){
+    if(window.scrollY > 1000 && !isShowing){
+        button.style.animation = '';
+        button.style.animation = 'in 0.15s linear 1 normal forwards';
+        isShowing = true;
+        console.log('In');
+    }
+    else if(window.scrollY < 1000 && isShowing){
+        button.style.animation = '';
+        button.style.animation = 'out 0.15s linear 1 normal forwards';
+        isShowing = false;
+        console.log('Out');
+    }
+}
+window.onscroll = CheckScroll;
--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-var folder = "../imgs/gallery/";
+var folder = "Origami/imgs/gallery/";
 let galleryArray = [];
+let count = 0;
+let grid = document.querySelector('.grid');
 
-for(let i = 0; i < 6; i++){
+for(let i = 0; i < 36; i++){
     galleryArray.push(folder + (i + 1) + "_cover.png");
 }
 
 LoadGallery();
 
 function LoadGallery(){
-    let i = 0;
+    console.log("Loading gallery images");
     document.querySelectorAll(".grid-block").forEach(function(block) {
-        block.style.backgroundImage = "url("+galleryArray[i]+")";
-        i++;
+        block.style.backgroundImage = "url(" + galleryArray[count] + ")";
+        count++;
     });
+    console.log("Success.");
 }
+
