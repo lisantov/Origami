ac84c77a254e4261a0b260deb78093f100f75d25
lisantov <71330868+lisantov@users.noreply.github.com>
2024-10-26 07:54:35 +0700

    Add files via upload

--- js/goTop.js
+++ js/goTop.js
@@ -2,13 +2,13 @@ let button = document.querySelector('.to-top');
 let isShowing = false;
 
 function CheckScroll(ev){
-    if(window.scrollY > 1000 && !isShowing){
+    if(window.scrollY > 800 && !isShowing){
         button.style.animation = '';
         button.style.animation = 'in 0.15s linear 1 normal forwards';
         isShowing = true;
         console.log('In');
     }
-    else if(window.scrollY < 1000 && isShowing){
+    else if(window.scrollY < 800 && isShowing){
         button.style.animation = '';
         button.style.animation = 'out 0.15s linear 1 normal forwards';
         isShowing = false;
